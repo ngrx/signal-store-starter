77f9bb812aa1b0aee02e1ed6246d96b52bba34c0
7s.i <7s.i@pm.me>
2026-01-16 20:21:42 +0800

    重構 HeaderComponent 組件，移動至新路徑並更新匯出路徑

--- src/app/shared/components/layouts/header/header.component.ts
+++ src/app/shared/components/layouts/header/header.component.ts
@@ -1,16 +1,16 @@
 import { Component, inject, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { AuthStore, AuthStoreInstance } from '../../../core/auth/stores/auth.store';
-import { ContextStore, ContextStoreInstance } from '../../../core/context/stores/context.store';
-import { MenuItem } from '../../models/menu.model';
-import { UserAvatarComponent } from '../../components/widgets/user-avatar/user-avatar.component';
-import { OrganizationAvatarComponent } from '../../components/widgets/user-avatar/organization-avatar.component';
-import { TeamAvatarComponent } from '../../components/widgets/user-avatar/team-avatar.component';
-import { PartnerAvatarComponent } from '../../components/widgets/user-avatar/partner-avatar.component';
-import { ContextSwitcherComponent } from '../../components/widgets/context-switcher/context-switcher.component';
-import { WorkspaceSwitcherComponent } from '../../components/widgets/workspace-switcher/workspace-switcher.component';
-import { ThemeToggleComponent } from '../../components/widgets/theme-toggle/theme-toggle.component';
+import { AuthStore, AuthStoreInstance } from '../../../../core/auth/stores/auth.store';
+import { ContextStore, ContextStoreInstance } from '../../../../core/context/stores/context.store';
+import { MenuItem } from '../../../models/menu.model';
+import { UserAvatarComponent } from '../../widgets/user-avatar/user-avatar.component';
+import { OrganizationAvatarComponent } from '../../widgets/user-avatar/organization-avatar.component';
+import { TeamAvatarComponent } from '../../widgets/user-avatar/team-avatar.component';
+import { PartnerAvatarComponent } from '../../widgets/user-avatar/partner-avatar.component';
+import { ContextSwitcherComponent } from '../../widgets/context-switcher/context-switcher.component';
+import { WorkspaceSwitcherComponent } from '../../widgets/workspace-switcher/workspace-switcher.component';
+import { ThemeToggleComponent } from '../../widgets/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-header',
