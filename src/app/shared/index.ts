0e3ed5b1eaad58037be86e4265688c5dd257ae90
7s.i <7s.i@pm.me>
2026-01-16 18:12:07 +0800

    fix: 修正匯出路徑，將 header.component 更新至正確的 layouts 目錄

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,5 +1,5 @@
 // Shared barrel exports
-export * from './components/header/header.component';
+export * from './layouts/header/header.component';
 export * from './services/avatar.service';
 export * from './services/menu.service';
 export * from './models/menu.model';
